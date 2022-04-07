b099cab7d09e7aba759119aa17b06e8d26fd01e8
Tejire-Elma <tejire.elma@gmail.com>
2022-04-07 11:23:19 -0700

    C program that prints

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main is the entry point
+ * always return 0 for success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
