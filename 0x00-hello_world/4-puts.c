7e9cba5807b50ebd7a180acc001a53b908b59b25
Tejire-Elma <tejire.elma@gmail.com>
2022-04-07 10:18:56 -0700

    Fixing puts.c14

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
-#include<stdio.h>
 /*
  * function main is the entry point*
  * Return is always 0 SUCCESS*
 */
+#include<stdio.h>
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
