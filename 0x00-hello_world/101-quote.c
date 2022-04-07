e6e82513608ed9967142b0dd2745bb5188f8ec8a
Tejire-Elma <tejire.elma@gmail.com>
2022-04-07 12:58:02 -0700

    C program that prints to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 /*
  * Auth: Tejiri
  */
-#include <stdio.h>
+#include <iostream>
 /**
  * main - this the starting point of out code
  * Return: 1
  */
 int main(void)
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	std::cerr << "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	return (1);
 }
