6c59458ed3363f8453e4433a6a8d62352b82ca81
Tejire-Elma <tejire.elma@gmail.com>
2022-04-07 11:52:37 -0700

    Task4 again

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
-#include <stdio.h>
 /*
- * function main is the entry point
- * Return is always 0 SUCCESS
+ * Auth: Tejiri
+ */
+#include <stdio.h>
+/**
+ * main - starting point of our c programme
+ * Return : 0
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
