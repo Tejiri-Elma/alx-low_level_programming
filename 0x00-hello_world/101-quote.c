e3c9222e672ece142dbc5c88b4f01493af600906
Tejire-Elma <tejire.elma@gmail.com>
2022-04-07 12:34:25 -0700

    C program that prints to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+/*
+ * Auth: Tejiri
+ */
+#include <stdio.h>
+/**
+ * main - this the starting point of out code
+ * Return: 1
+ */
+int main(void)
+{
+	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
