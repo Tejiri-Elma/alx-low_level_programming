43fc65727c9c3f356caa35a7cf05c88f0a61294c
Tejire-Elma <tejire.elma@gmail.com>
2022-04-07 09:14:16 -0700

    Fixing puts.c11

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include<stdio.h>
-int main(void) /* function main is the entry point */
+int main(void)
+/*
+ * function main is the entry point*
+ * Return is always 0 SUCCESS*
+ */
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
