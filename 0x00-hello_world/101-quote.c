a860a75752236cee65d3a72e4ab7323b22a747cf
Tejire-Elma <tejire.elma@gmail.com>
2022-04-07 13:32:34 -0700

    C program that prints to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,18 +6,15 @@
 #include <unistd.h>
 
 /**
- *
  * main - Entry point
- *
  * Return:1
- *
-*/
+ */
 
 int main(void)
 
 {
 
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 
 	return (1);
 
