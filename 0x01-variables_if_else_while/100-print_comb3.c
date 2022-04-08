93e51a4cbd03a99b0c1fe89398bd2cccc7b316f5
Tejire-Elma <tejire.elma@gmail.com>
2022-04-08 12:31:26 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,10 +15,8 @@ int main(void)
 		{
 			putchar((i % 10) + '0');
 			putchar((j % 10) + '0');
-			if (i == 0 && j == 9);
-			{
+			if (i == 0 && j == 9)
 				continue;
-			}
 			putchar(',');
 			putchar(' ');
 		}
