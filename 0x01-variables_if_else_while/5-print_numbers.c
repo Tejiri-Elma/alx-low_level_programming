b32eb3817f0bdaf4a3cd05016750ea93b8480fd8
Tejire-Elma <tejire.elma@gmail.com>
2022-04-08 09:52:19 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+/*
+ * Auth: Tejiri
+ */
+#include <stdio.h>
+/**
+ * main - this is the entry point of the code
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		printf("%d\n", a);
+	}
+	return (0);
+}
