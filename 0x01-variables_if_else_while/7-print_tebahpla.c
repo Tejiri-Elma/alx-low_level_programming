d5d8838cb9ffd52c8ba81ecf5d1b1441b82e16d5
Tejire-Elma <tejire.elma@gmail.com>
2022-04-08 10:45:22 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+/*
+ * Auth: Tejiri
+ */
+#include <stdio.h>
+/**
+ * main - this is the entry point of the code
+ * Return: 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'z', i <= 'a', i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
