aadcb7fd6618acdc0e9693514511ad9beaf22e9d
Tejire-Elma <tejire.elma@gmail.com>
2022-04-08 09:38:27 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+/*
+ * Auth: Tejiri
+ */
+#include <stdio.h>
+/**
+ * main - This is the entry point of the code
+ * Return: 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z', i++)
+	{
+		if (i == 'q')
+		{
+			continue;
+		}
+		else if (i == 'e')
+		{
+			continue;
+		}
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
+
