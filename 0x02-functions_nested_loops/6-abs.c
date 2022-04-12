d10af15e99a929fd07d5bc086b444fac9a5420f4
Tejire-Elma <tejire.elma@gmail.com>
2022-04-12 10:54:29 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,25 @@
+/*
+ * Auth - Tejiri
+ * a function that computes the absolute value of an integer
+ */
+#include "main.h"
+#include <stdio.h>
+/**
+ * _abs - checks the absolue value of an integer
+ * Return: 0
+ */
+int _abs(int)
+{
+	if (n < 0)
+	{
+		return (n * (-1));
+	}
+	else if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (n);
+	}
+}
