240d40f19a3d23a5afcf8d7a19d9a5a248905540
Tejire-Elma <tejire.elma@gmail.com>
2022-04-12 09:47:33 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,13 +7,14 @@
  * main - this is the entry point of the code
  * Return: 0
  */
-void print_alphabet(void);
-{
-	char i;
+void print_alphabet(void)
 
-	for (i = 'a'; i <= 'z'; i++)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
-		_putchar(i);
+		_putchar(alphabet);
 	}
 	_putchar('\n');
 }
