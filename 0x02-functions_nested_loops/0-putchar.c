cd821772fedaf2b8d27befbddc5efcca9f9d16f8
Tejire-Elma <tejire.elma@gmail.com>
2022-04-12 09:19:20 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+/*
+ * Auth: Tejiri
+ * a program that prints _putchar, followed by a new line
+ */
+#include "main.h"
+/**
+ * main - this is the entry point of the code
+ * Return: 0
+ */
+int main(void)
+{
+	_putchar("_putchar");
+	_putchar('\n');
+	return (0);
+}
