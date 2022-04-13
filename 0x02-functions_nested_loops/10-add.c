02e222f56f979d3f4b031383a9f4139ea4da2fd6
Tejire-Elma <tejire.elma@gmail.com>
2022-04-12 17:05:38 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+/*
+ * Auth - Tejiri
+ * a function that adds two integers and returns the result
+ */
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - adds 2 numbers
+ * Return: 0
+ * @a :print int a
+ * @b: print int b
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
