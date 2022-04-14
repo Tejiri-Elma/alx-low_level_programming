f39645cfdf3c9a57bcf1d9fa4bc18f0987274cee
Tejire-Elma <tejire.elma@gmail.com>
2022-04-14 02:19:13 -0700

    Task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ * Return: 0
+ */
+void more_numbers(void)
+{
+	int c;
+
+	i = 0;
+	while (i < 10)
+	{
+		for (c = 0; c <= 14; c++)
+		{
+			_putchar(c + '0');
+		}
+		_putchar('\n');
+		i++;
+	}
+	_putchar('\n');
+}
