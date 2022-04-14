19645f2fdce61da323487564518a2382070c4488
Tejire-Elma <tejire.elma@gmail.com>
2022-04-13 22:04:05 -0700

    Task 15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -3,19 +3,19 @@
  *main - print first 50 fibonacci
  *Return: 0 always.
  */
-int main(void) 
+int main(void)
 {
-  long int i, x = 1, y = 2, sum = 0, tSum = 0;
-  for (i = 0; i < 49; i++)
-    {
-      if ((y % 2 == 0) && (y <= 4000000))
-	{
-	  tSum = tSum + y; 
-	}
-      sum = x + y;
-      x = y;
-      y = sum; 
-    }
-  printf("%ld\n", tSum);
-  return (0);
+		long int i, x = 1, y = 2, sum = 0, tSum = 0;
+
+		for (i = 0; i < 49; i++)
+		{
+			if ((y % 2 == 0) && (y <= 4000000))
+			{											tSum = tSum + y;
+												 	}
+			sum = x + y;
+			x = y;
+			y = sum;
+			printf("%ld\n", tSum);
+		}
+		return (0);
 }
