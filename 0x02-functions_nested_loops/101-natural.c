57c8e1f9443f3fe2d87d2e0e6f91c7c0f8748745
Tejire-Elma <tejire.elma@gmail.com>
2022-04-13 21:50:37 -0700

    Task 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,14 +6,14 @@
  */
 int main(void)
 {
-	int i, res;
 
+	int i, res;
 	for (i = 0; i < 1024; i++)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
 		{
-			res += i;
-			printf("%d\n", res);
+		res += i;
+		printf("%d\n", res);
 		}
 	}
 	return (0);
