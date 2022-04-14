cfff89aa5826ba287c826783f77ca6d54d32ef5e
Tejire-Elma <tejire.elma@gmail.com>
2022-04-14 01:20:49 -0700

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+/**
+ * mul - multiplies 2 digits
+ * Return: 0
+ * @a: digit to be multiplied
+ * @b: digit to be multiplied
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (0);
+}
+int main(void)
+{
+	return (0);
+}
