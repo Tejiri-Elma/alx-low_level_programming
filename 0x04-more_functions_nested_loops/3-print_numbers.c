5df93bcf7e2ebdd192429b9eeb57ae51c4dfca94
Tejire-Elma <tejire.elma@gmail.com>
2022-04-14 01:54:17 -0700

    Task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,13 +2,13 @@
  * print_numbers - prints the numbers, from 0 to 9
  * Return: 0
  */
-void print_numbers(void);
+void print_numbers(void)
 {
 	int c;
 
-	if (c = 0; c >= 9; c++)
+	if (c = 0; c <= 9; c++)
 	{
-		_putchar(c);
+		_putchar(c + '0');
 	}
 	_putchar('\n');
 	return (0);
