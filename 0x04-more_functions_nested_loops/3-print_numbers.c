ee957158991608963baddbe2ae77f4685ac945a3
Tejire-Elma <tejire.elma@gmail.com>
2022-04-14 01:39:24 -0700

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+/**
+ * print_numbers - prints the numbers, from 0 to 9
+ * Return: 0
+ */
+void print_numbers(void);
+{
+	int c;
+
+	if (c = 0; c >= 9; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+	return (0);
+}
+int main()
+{
+	return (1);
+}
