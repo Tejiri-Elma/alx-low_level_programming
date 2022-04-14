315bab316f8192cea66b44183c9ffcf97126b9b7
Tejire-Elma <tejire.elma@gmail.com>
2022-04-14 02:35:40 -0700

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+include "main.h"
+/**
+ * print_line -  draws a straight line in the terminal
+ * Retrun: Nothing
+ * @n: draws the line
+ */
+void print_line(int n);
+{
+	if (n > 0)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+	else
+	{
+		_putchar('\n');
+	}
+}
