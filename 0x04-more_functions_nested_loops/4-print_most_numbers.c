f0e903d525b21fa7944ecd8396cf3cf1247c82b4
Tejire-Elma <tejire.elma@gmail.com>
2022-04-14 02:05:21 -0700

    Task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9
+ * except 2 and 4
+ * Return: Nothing
+ */
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		if (c != 2 && c != 4)
+		{
+			_putchar(c + '0');
+		}
+	}
+	_putchar('\n');
+}
+int main()
+{
+	return (0);
+}
