e0a9f6dc464564af279e44006dccc1622b8377ed
Tejire-Elma <tejire.elma@gmail.com>
2022-04-13 21:57:21 -0700

    Task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,13 +5,13 @@
  * followed by a new line
  * Return: ALways 0 (Success)
  */
-int main(void) 
+int main(void)
 {
 	unsigned long int i, j, k, j1, j2, k1, k2;
 
 	j = 1;
 	k = 2;
-	printf("%lu", j); 
+	printf("%lu", j);
 	for (i = 1; i < 91; i++)
 	{
 		printf(", %lu", k);
