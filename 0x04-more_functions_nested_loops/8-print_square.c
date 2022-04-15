d9f6a8b5459e19938c941d5b0e08ae0f936f4786
Tejire-Elma <tejire.elma@gmail.com>
2022-04-15 02:05:52 -0700

    Task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -3,15 +3,16 @@
  * print_square - print the squares of #
  * @size: size of the square
  */
+int _putchar(char c);
 void print_square(int size)
 {
 	int a, b;
 
 	if (size > 0)
 	{
-		for (a = 0; a <= size; a++)
+		for (a = 0; a < size; a++)
 		{
-			for (b = 0; b <= size; b++)
+			for (b = 0; b < size; b++)
 			{
 				_putchar(35);
 			}
