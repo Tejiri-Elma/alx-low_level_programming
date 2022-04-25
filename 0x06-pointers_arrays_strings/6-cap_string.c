cd194df772b395210309420fa67866e53dd26cfa
Tejire-Elma <tejire.elma@gmail.com>
2022-04-24 17:41:05 -0700

    Task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes the first letters of all words of a string
+ * @s: string
+ * Return: s
+ */
+char *cap_string(char *s)
+{
+	int i, j = 0;
+
+	char separators[] = {' ', '\t', '\n', ',', ';',
+		'.', '!', '?', '"', '(', ')', '{', '}'};
+
+	i = 0;
+
+	if ((s[i] >= 'a') && (s[i] <= 'z'))
+	{
+		s[i] = s[i] - 32;
+	}
+
+	while (s[i] != '\0')
+	{
+		for (i = 0; i < j; i++)
+		{
+			if (s[i] == separators[j])
+			{
+				if ((s[i + 1] >= 'a') && (s[i + 1] <= 'z'))
+				{
+					s[i + 1] = s[i + 1] - 32;
+					break;
+				}
+				j++;
+			}
+		}
+	}
+	return (s);
+}
