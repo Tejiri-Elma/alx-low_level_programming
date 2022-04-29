a00b56295a5bff619a49093e097178175f1fdbfc
Tejire-Elma <tejire.elma@gmail.com>
2022-04-29 04:55:40 -0700

    Task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -22,7 +22,8 @@ int _sqrt(int a, int b)
 	}
 }
 /**
- * _sqrt_recursion - a function that returns the natural square root of a number
+ * _sqrt_recursion - a function that
+ * returns the natural square root of a number
  * @n: integer
  * Return: square root of n on success
  * On error, return -1
@@ -31,7 +32,7 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 	{
-		return(-1);
+		return (-1);
 	}
 	return (_sqrt(n, 1));
 }
