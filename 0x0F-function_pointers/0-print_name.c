9625b5b16fb5037bddcc3a4f7c87c231e30d2ad9
Tejire-Elma <tejire.elma@gmail.com>
2022-05-16 03:57:16 -0700

    Task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+#include "stdio.h"
+/**
+ * print_name - a function that prints a name
+ * @name: name to be printed
+ * @f: call
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
