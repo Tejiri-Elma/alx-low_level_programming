ce0810a83018267770044151809bc965c6613120
Tejire-Elma <tejire.elma@gmail.com>
2022-05-17 05:39:50 -0700

    Task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,12 +12,11 @@ int sum_them_all(const unsigned int n, ...)
 	va_list args;
 
 	va_start(args, n);
-	
 	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(args, int);
 	}
-	if ( n == 0)
+	if (n == 0)
 		return (0);
 	va_end(args);
 	return (sum);
