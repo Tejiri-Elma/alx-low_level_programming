7408015c95193bf6e764161886991bbccda4d5e5
Tejire-Elma <tejire.elma@gmail.com>
2022-05-17 05:04:51 -0700

    Task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -9,8 +9,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list args;
-	
+
 	va_start(args, n);
+
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(args, unsigned int));
