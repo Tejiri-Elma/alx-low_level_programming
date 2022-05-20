5c1ff8b9b380adc06b22e970140e65e0bfd15537
Tejire-Elma <tejire.elma@gmail.com>
2022-05-20 09:08:32 -0700

    Task 3

---
+++ 0x0E-structures_typedef/3-op_functions.c
@@ -0,0 +1,52 @@
+#include "3-calc.h"
+#include <stdlib.h>
+/**
+ * op_add - add 2 integers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: a+b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subtract 2 integers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: a-b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplies 2 integers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: a*b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divides 2 integers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: a/b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - gives the remainder from the division of 2 numbers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: a%b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
