3e83e49eb9baacf236da14f157c581016f653ff0
Tejire-Elma <tejire.elma@gmail.com>
2022-05-20 01:21:52 -0700

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a new struct
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ * Description: this struct contains 3 elements
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
