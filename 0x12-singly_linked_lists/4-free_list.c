008e94d36ec115fbd0244b2edc86add0bddefb85
Tejire-Elma <tejire.elma@gmail.com>
2022-05-25 12:43:31 -0700

    Task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_list - functon that prints a list
+ * @head: beginning og linked list
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
