e87c2cd0707c268453284e349fa425e6e6ccb907
Tejire-Elma <tejire.elma@gmail.com>
2022-05-25 08:47:36 -0700

    Task 0

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,27 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+
+/**
+ * struct list_t - struct for singly linked list
+ * @str: string element of list
+ * @len: length of the string as an elemnt of the list
+ * @next: points to the next node
+ */
+typedef struct list_t
+{
+	char *str;
+	unsigned int len;
+	struct list_t *next;
+} list_t;
+
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
+#endif
