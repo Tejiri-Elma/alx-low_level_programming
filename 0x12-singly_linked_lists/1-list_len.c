0d91fd3e473a981c6d30f3c13a6f8029fbf4ce6d
Tejire-Elma <tejire.elma@gmail.com>
2022-05-25 09:04:51 -0700

    Task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+/**
+ * list_len - function that returns the number of elements in a linked list
+ * @h: head of a node in a linked list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t i;
+
+	for (i = 0; h != NULL; i++)
+		h = h->next;
+	return (i);
+}
