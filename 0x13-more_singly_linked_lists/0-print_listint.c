9aafcb81fb3f1de540a070efd3bbb1de618e6fab
Tejire-Elma <tejire.elma@gmail.com>
2022-05-26 01:52:55 -0700

    Task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * print_listint - prints all the elements of a list
+ * @h: head of a linked list
+ * Return: the number of elements in a list
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t i;
+
+	for (i = 0; h != NULL; i++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (i);
+}
