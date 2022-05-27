787b65f9cbaf5c1a9ee2d022f1f0809b2e7e0540
Tejire-Elma <tejire.elma@gmail.com>
2022-05-27 04:15:36 -0700

    Task 4

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -5,14 +5,8 @@
  */
 void free_listint(listint_t *head)
 {
-	listint_t *temp;
-
 	if (head == NULL)
 		return;
-	while (head->next != NULL)
-	{
-		temp = head->next;
-		free(head);
-		head = temp;
-	}
+	free_listint(head->next);
+	free(head);
 }
